d6d535e57b782d3811e57c7cd2b4d09349884098
nawal addou <nawal.addou@gmail.com>
2013-05-08 01:25:01 +0100

    variable

---
+++ variable/variable.cpp
@@ -0,0 +1,26 @@
+// variable.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int ageUtilisateur(16);    
+    int nombreAmis(401);      
+ 
+    double pi(3.14159);
+ 
+    bool tumaime(true);    
+ 
+    char lettre('N');
+    cout << "Votre age est : " << ageUtilisateur << endl;
+	cout << "Le nombre de tes amis sur face lool : " << nombreAmis << endl;
+	cout << "sdamtni!!!!!!! " << endl;
+	cout << "la valeur de PI est : " << pi << endl;
+	cout << "est ce ke tu m'aime bcp : " << tumaime << endl;
+	cout << "Notre lettre préfèré : " << lettre << endl;
+    return 0;
+}
